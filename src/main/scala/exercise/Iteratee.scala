b0ccd139906228a51a65421f5bed4818faa074ac
Nilanjan Raychaudhuri <nraychaudhuri@gmail.com>
2013-07-05 19:29:12 -0400

    more clean up

--- src/main/scala/exercise/Iteratee.scala
+++ src/main/scala/exercise/Iteratee.scala
@@ -6,27 +6,13 @@ object Iteratee {
   
   trait SolutionIteratee[E, +A] extends Iteratee[E, A]{
 
-    def run: A = fold {
-      case Step.Done(a, _) => a
-      case Step.Cont(k) => k(EOF).fold {
-        case Step.Done(a, _) => a
-        case Step.Cont(_) => sys.error("diverging iteratee after Input.EOF")
-      }
-    }
+    def run: A = ???
 
-    def flatMap[B](f: A => Iteratee[E, B]): Iteratee[E, B] = this match {
-      case Done(a, e) => f(a) match {
-        case Done(a, _) => Done(a, e)
-        case Cont(k) => k(e)
-      }
-      case Cont(k) => Cont(e => k(e) flatMap f)
-    }
+    def flatMap[B](f: A => Iteratee[E, B]): Iteratee[E, B] = ???
 
-    def map[B](f: A => B): Iteratee[E, B] = this match {
-      case Done(a, e) => Done(f(a), e)
-      case Cont(k) => Cont(e => k(e) map f)
-    }
+    def map[B](f: A => B): Iteratee[E, B] = ???
   }
+  
   case class Done[E, +A](state: A, remaining: Input[E]) extends SolutionIteratee[E, A] {
     def fold[B](folder: Step[E, A] => B): B = folder(Step.Done(state, remaining))
   }
